011b8587a12541d178ba2977356e66112c342b43
Matheus-Reinert <reinert.matheus@hotmail.com>
2021-02-10 18:19:12 -0300

    first commit

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+const UP = Vector2(0,-1)
+var motion = Vector2()
+const GRAVITY = 20
+const SPEED = 200
+const  JUMP_HEIGHT = -550
+
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+	else: 
+		motion.x = 0;
+		
+	if is_on_floor():
+		print("Esta no chao")
+		if Input.is_action_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+			
+	motion = move_and_slide(motion, UP)	
---
+++ README.md
@@ -0,0 +1,1 @@
+# Godot_Jogo2D
